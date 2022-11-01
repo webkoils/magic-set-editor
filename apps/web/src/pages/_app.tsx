bd5a76813d5a07521d9f36c0dc9ca3fdd073c8b4
John Anderson <john@scener.com>
2022-10-31 23:19:59 -0700

    more fixes

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -2,6 +2,8 @@ import { MseThemeProvider } from 'components/MseThemeProvider';
 import { AppProps } from 'next/app';
 import '@mse/fonts.beleren/index.css';
 import '@mse/fonts.mplantin/latin.css';
+import '@mse/templates.m15/m15.css';
+
 function MyApp({ Component, pageProps }: AppProps) {
   const Comp = Component as any;
   return (
